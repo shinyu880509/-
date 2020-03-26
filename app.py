0afd0ae2a8152b4ad709e31a63b830a7aaf7391c
josephLu <10646021@ntub.edu.tw>
2020-03-27 01:09:56 +0800

    0327update
    0327update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config["JSON_AS_ASCII"] = False
 
 @app.route("/")
 def home():
-    return render_template('index.html',datelist2 = datelist, closelist2 = closelist) 
+    return render_template('index.html',datelist2 = datelist, closelist2 = closelist,re = re) 
    
 itStock = ['2427']
 stockID = itStock[0]
@@ -26,6 +26,31 @@ date = np.array(table.date)
 datelist = date.tolist()
 close = np.array(table.close)
 closelist = close.tolist()
+
+capacity = np.array(table.capacity)
+capacitylist = capacity.tolist()
+turnover = np.array(table.turnover)
+turnoverlist = turnover.tolist()
+change = np.array(table.change)
+changelist = change.tolist()
+trans = np.array(table.transaction)
+translist = trans.tolist()
+
+re = []
+a = sum(capacitylist)
+re.append(str(a))
+a = sum(turnoverlist)
+re.append(str(a))
+a = sum(translist)
+re.append(str(a))
+a = max(closelist)
+re.append(str(a))
+a = min(closelist)
+re.append(str(a))
+b = round(sum(changelist),2)
+re.append(str(b))
+b = round(sum(closelist)/len(closelist),2)
+re.append(str(b))
 
 
 if __name__ == "__main__":

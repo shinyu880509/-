bd2a7bae0edcac1f7cdc9e20a56fc9653921d66a
ShuoLi <10646015@ntub.edu.tw>
2020-03-29 21:56:46 +0800

    update app.py and index.html
    create BarChart

--- app.py
+++ app.py
@@ -2,24 +2,25 @@ from flask import Flask, request, render_template
 import pandas as pd
 import csv
 import numpy as np
+import twstock
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 
 @app.route("/")
 def home():
-    return render_template('index.html',datelist2 = datelist, closelist2 = closelist,re = re) 
+    return render_template('index.html',datelist2 = datelist, closelist2 = closelist,translist2 = translist,re = re) 
    
 itStock = ['2427']
 stockID = itStock[0]
-'''
+
 stock = twstock.Stock(stockID)
 
 stockFetch = stock.fetch_31()
 stockDf = pd.DataFrame(stockFetch)
 stockDf.set_index('date', inplace = True)
 stockDf.to_csv(stockID+'.csv')
-'''
+
 table = pd.read_csv(stockID+'.csv')
 
 date = np.array(table.date)

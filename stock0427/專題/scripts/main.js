570b32dcd49c198dd21a1886b617cf9571d949da
1999hys <10646026@ntub.edu.tw>
2020-04-30 23:08:20 +0800

    update stock
    update stock

---
+++ stock0427/專題/scripts/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    // 滑鼠移入	
+    $('.stockSearch').click(function(){        
+        $('.allstock').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.chart').click(function(){        
+        $('.history').toggle(); 
+    });	
+
+});

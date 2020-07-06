35ccbec9c4d0105af7bf13929f35b368a8889b01
ShouLi15 <10646015@ntub.edu.tw>
2020-07-06 22:27:36 +0800

    0706
    創建資料庫

---
+++ sqlite.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+#連結資料庫
+conn = sqlite3.connect('stock.db')
+c =conn.cursor()
+
+#創建資料表
+
+c.execute('''CREATE TABLE account
+       (username  CHAR(50)  PRIMARY KEY     NOT NULL,
+        password  CHAR(50)    NOT NULL,
+        mail      CHAR(50)     NOT NULL);''')
+
+c.execute('''CREATE TABLE attention
+       (username  CHAR(50)  PRIMARY KEY     NOT NULL,
+        attention  CHAR(50)    NOT NULL);''')
+
+
+
+c.execute('''CREATE TABLE verification
+      (username  CHAR(50)  PRIMARY KEY     NOT NULL,
+       mail      CHAR(50)     NOT NULL,
+       verification CHAR(50));''')
+
+conn.commit()
+conn.close()

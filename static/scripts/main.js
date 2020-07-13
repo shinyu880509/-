811d450a429671712c613103650bdd724df8f757
1999hys <10646026@ntub.edu.tw>
2020-07-13 16:39:39 +0800

    0713
    點選按鈕加入我的最愛

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -13,11 +13,99 @@ $(function(){
     });	
 
 });
-
+//添加我的最愛功能
 $(function(){
     // 滑鼠移入	
     $('.button2427').click(function(){        
         $('.2427text').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.button2453').click(function(){        
+        $('.2453text').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.button2468').click(function(){        
+        $('.2468text').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.button2471').click(function(){        
+        $('.2471text').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.button2480').click(function(){        
+        $('.2480text').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.button3029').click(function(){        
+        $('.3029text').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.button3130').click(function(){        
+        $('.3130text').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.button4994').click(function(){        
+        $('.4994text').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.button5203').click(function(){        
+        $('.5203text').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.button6112').click(function(){        
+        $('.6112text').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.button6183').click(function(){        
+        $('.6183text').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.button6214').click(function(){        
+        $('.6214text').toggle(); 
     });	
 
 });

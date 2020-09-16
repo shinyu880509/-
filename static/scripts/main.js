a31b6284469f887d4165dadf0893b899c09dbc28
jjfj3750 <jjfj3750@gmail.com>
2020-09-16 14:39:52 +0800

    0916
    我的最愛增加點選消失和減號

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -18,6 +18,9 @@ $(function(){
     // 滑鼠移入	
     $('.button2427').click(function(){        
         $('.2427text').toggle(); 
+        $('.but2427').toggle(); 
+        $('.text2427').toggle(); 
+        $('.button2427').toggle(); 
     });	
 
 });
@@ -26,6 +29,9 @@ $(function(){
     // 滑鼠移入	
     $('.button2453').click(function(){        
         $('.2453text').toggle(); 
+        $('.but2453').toggle(); 
+        $('.text2453').toggle(); 
+        $('.button2453').toggle(); 
     });	
 
 });
@@ -34,6 +40,9 @@ $(function(){
     // 滑鼠移入	
     $('.button2468').click(function(){        
         $('.2468text').toggle(); 
+        $('.but2468').toggle(); 
+        $('.text2468').toggle(); 
+        $('.button2468').toggle(); 
     });	
 
 });
@@ -42,6 +51,9 @@ $(function(){
     // 滑鼠移入	
     $('.button2471').click(function(){        
         $('.2471text').toggle(); 
+        $('.but2471').toggle(); 
+        $('.text2471').toggle(); 
+        $('.button2471').toggle(); 
     });	
 
 });
@@ -50,6 +62,9 @@ $(function(){
     // 滑鼠移入	
     $('.button2480').click(function(){        
         $('.2480text').toggle(); 
+        $('.but2480').toggle(); 
+        $('.text2480').toggle(); 
+        $('.button2480').toggle(); 
     });	
 
 });
@@ -58,6 +73,9 @@ $(function(){
     // 滑鼠移入	
     $('.button3029').click(function(){        
         $('.3029text').toggle(); 
+        $('.but3029').toggle(); 
+        $('.text3029').toggle(); 
+        $('.button3029').toggle(); 
     });	
 
 });
@@ -66,6 +84,9 @@ $(function(){
     // 滑鼠移入	
     $('.button3130').click(function(){        
         $('.3130text').toggle(); 
+        $('.but3130').toggle(); 
+        $('.text3130').toggle(); 
+        $('.button3130').toggle(); 
     });	
 
 });
@@ -74,6 +95,9 @@ $(function(){
     // 滑鼠移入	
     $('.button4994').click(function(){        
         $('.4994text').toggle(); 
+        $('.but4994').toggle(); 
+        $('.text4994').toggle(); 
+        $('.button4994').toggle(); 
     });	
 
 });
@@ -82,6 +106,9 @@ $(function(){
     // 滑鼠移入	
     $('.button5203').click(function(){        
         $('.5203text').toggle(); 
+        $('.but5203').toggle(); 
+        $('.text5203').toggle(); 
+        $('.button5203').toggle(); 
     });	
 
 });
@@ -90,6 +117,9 @@ $(function(){
     // 滑鼠移入	
     $('.button6112').click(function(){        
         $('.6112text').toggle(); 
+        $('.but6112').toggle(); 
+        $('.text6112').toggle(); 
+        $('.button6112').toggle(); 
     });	
 
 });
@@ -98,6 +128,9 @@ $(function(){
     // 滑鼠移入	
     $('.button6183').click(function(){        
         $('.6183text').toggle(); 
+        $('.but6183').toggle(); 
+        $('.text6183').toggle(); 
+        $('.button6183').toggle(); 
     });	
 
 });
@@ -106,6 +139,141 @@ $(function(){
     // 滑鼠移入	
     $('.button6214').click(function(){        
         $('.6214text').toggle(); 
+        $('.but6214').toggle(); 
+        $('.text6214').toggle(); 
+        $('.button6214').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but2427').click(function(){        
+        $('.2427text').toggle(); 
+        $('.but2427').toggle(); 
+        $('.text2427').toggle(); 
+        $('.button2427').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but2453').click(function(){        
+        $('.2453text').toggle(); 
+        $('.but2453').toggle(); 
+        $('.text2453').toggle(); 
+        $('.button2453').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but2468').click(function(){        
+        $('.2468text').toggle(); 
+        $('.but2468').toggle(); 
+        $('.text2468').toggle(); 
+        $('.button2468').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but2471').click(function(){        
+        $('.2471text').toggle(); 
+        $('.but2471').toggle(); 
+        $('.text2471').toggle(); 
+        $('.button2471').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but2480').click(function(){        
+        $('.2480text').toggle(); 
+        $('.but2480').toggle(); 
+        $('.text2480').toggle(); 
+        $('.button2480').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but3029').click(function(){        
+        $('.3029text').toggle(); 
+        $('.but3029').toggle(); 
+        $('.text3029').toggle(); 
+        $('.button3029').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but3130').click(function(){        
+        $('.3130text').toggle(); 
+        $('.but3130').toggle(); 
+        $('.text3130').toggle(); 
+        $('.button3130').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but4994').click(function(){        
+        $('.4994text').toggle(); 
+        $('.but4994').toggle(); 
+        $('.text4994').toggle(); 
+        $('.button4994').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but5203').click(function(){        
+        $('.5203text').toggle(); 
+        $('.but5203').toggle(); 
+        $('.text5203').toggle(); 
+        $('.button5203').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but6112').click(function(){        
+        $('.6112text').toggle(); 
+        $('.but6112').toggle(); 
+        $('.text6112').toggle(); 
+        $('.button6112').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but6183').click(function(){        
+        $('.6183text').toggle(); 
+        $('.but6183').toggle(); 
+        $('.text6183').toggle(); 
+        $('.button6183').toggle(); 
+    });	
+
+});
+
+$(function(){
+    // 滑鼠移入	
+    $('.but6214').click(function(){        
+        $('.6214text').toggle(); 
+        $('.but6214').toggle(); 
+        $('.text6214').toggle(); 
+        $('.button6214').toggle(); 
     });	
 
 });

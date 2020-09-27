c243a473e1206ff080a3c03f64e5e5bb8a964b79
ShouLi15 <10646015@ntub.edu.tw>
2020-09-28 01:07:40 +0800

    0928
    當日股價寫入當日csv

--- twstocktest.py
+++ twstocktest.py
@@ -2,6 +2,7 @@ import time
 import pandas
 import sched
 import twstock
+import os
 
 
 #s = sched.scheduler(time.time, time.sleep)
@@ -9,12 +10,19 @@ import twstock
 itStock = ['2427', '2453', '2468', '2471', '2480', '3029', '3130', '4994', '5203', '6112', '6183', '6214']
 realtime = twstock.realtime.get(itStock)
 
+print(realtime)
+time = time.strftime('%Y%m%d')
+
 for i in range(len(itStock)):
     stockID = itStock[i]
     stock = {**realtime[stockID]['info'],**realtime[stockID]['realtime']}
     columns = ['time','latest_trade_price','trade_volume','accumulate_trade_volume','open','high','low']
     df = pandas.DataFrame([stock], columns = columns, index = [0])
-    df.to_csv('catStock/' + stockID + 'test.csv', index = False,encoding = 'utf_8_sig')
+    target_file = f'catStock/{time}-{stockID}.csv'
+    if os.path.exists(target_file):
+        df.to_csv('catStock/' + time + '-' + stockID + '.csv', mode='a',header=False , index = False,encoding = 'utf_8_sig')
+    else:
+        df.to_csv('catStock/' + time + '-' + stockID + '.csv' , index = False,encoding = 'utf_8_sig')
 
 #time.sleep(5)
 
